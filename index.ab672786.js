4ad835f9cc93ee6c4788be01cec2a1a17350dc90
AnastasiaVerbenets <anastasiaverbenets@gmail.com>
2024-07-04 17:21:43 +0000

    Deploying to gh-pages from @ AnastasiaVerbenets/parcel@f089edc336d60a2fe0885c7db49cb1de82535fc3 🚀

--- index.ab672786.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedbackForm").addEventListener("submit",(function(e){e.preventDefault();var a,t=e.currentTarget.elements,m=t.username.value,n=t.email.value,r=t.message.value;a={name:m,email:n,message:r},console.log("Form data",a)}));
-//# sourceMappingURL=index.ab672786.js.map
